b8039c85dae8c2fbf0ab6d2916a69646b785bd5f
IvannaBilyar <ivankabilyar2@gmail.com>
2022-09-19 12:50:07 +0300

    JAVA

---
+++ TestVariant1.java
@@ -0,0 +1,168 @@
+package lab0;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestVariant1 {
+
+//    Variant1 ob1 = new Variant1();
+
+
+    @Test(dataProvider = "integerProvider")
+    public void testInputTask(int p1, int p3) {
+        assertEquals(new Variant1().integerTask(525, 100), 5);
+    }
+
+    @DataProvider
+    public Object[][] integerProvider() {
+        return new Object[][]{{200, 100}, {13, 100}};
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void negativeIntegerTest() {
+        new Variant1().integerTask(-2, 0);
+    }
+
+    ////////////////////////////////////////////////
+
+    @Test(dataProvider = "booleanProvider")
+    public void testBooleanTask(int p1, boolean p3) {
+        assertEquals(new Variant1().booleanTask(p1), p3);
+    }
+
+    @DataProvider
+    public Object[][] booleanProvider() {
+        return new Object[][]{{7, true}, {0, false}, {-1, false}};
+    }
+
+    //////////////////////////////////////////////////
+
+    @Test(dataProvider = "ifProvider")
+    public void testIfTask(int p1, int p3) {
+        assertEquals(new Variant1().ifTask(p1), p3);
+    }
+
+    @DataProvider
+    public Object[][] ifProvider() {
+        return new Object[][]{{5, 6}, {0, 0}, {-1, -1}};
+    }
+
+    //////////////////////////////////////////////////
+
+
+    @Test(dataProvider = "caseProvider")
+    public void testCaseTask(int p1, String p2) {
+        assertEquals(new Variant1().caseTask(p1), p2);
+    }
+
+    @DataProvider
+    public Object[][] caseProvider() {
+        return new Object[][]{{3, "Wednesday"}, {6, "Saturday"}, {1, "Monday"}};
+    }
+
+    @Test(expectedExceptions = AssertionError.class)
+    public void negativeCaseTest() {
+        new Variant1().caseTask(8);
+    }
+
+
+//    @Test(dataProvider = "caseProvider")
+//    public void testCaseTask(int p1, DAY_OF_WEEK p2) {
+//        assertEquals(new Variant1().caseTask(p1), p2);
+//    }
+//
+//    @DataProvider
+//    public Object[][] caseProvider() {
+//        return new Object[][]{{3, DAY_OF_WEEK.WEDNESDAY}, {1, DAY_OF_WEEK.MONDAY}};
+//    }
+//
+//    @Test(expectedExceptions = AssertionError.class)
+//    public void negativeCaseTest() {
+//        new Variant1().caseTask(8);
+//    }
+
+
+
+    ///////////////////////////////////////////////////
+
+    @Test(dataProvider = "forProvider")
+    public void forTest(int p1, int p2, int p3) {
+        assertEquals(new Variant1().forTask(p1, p2), p3);
+    }
+
+    @DataProvider
+    public Object[][] forProvider() {
+        return new Object[][]{{13, 4, 4}, {7, 2, 2}};
+    }
+
+
+    @Test(expectedExceptions = AssertionError.class, dataProvider = "incorrectNProvider")
+    public void forTaskIncorrectNTest(int incorrectN) {
+        new Variant1().forTask(8, incorrectN);
+    }
+
+    @DataProvider
+    public Object[][] incorrectNProvider() {
+        return new Object[][]{{-20}, {0}, {-1}};
+    }
+
+    ///////////////////////////////////////////////////
+
+
+    @Test(dataProvider = "whileProvider")
+    public void testWhileTask(int a, int b, int c) {
+        assertEquals(new Variant1().whileTask(a, b), c);
+    }
+
+    @DataProvider
+    public Object[][] whileProvider() {
+        return new Object[][]{{13, 10, 3}, {10, 9, 1}, {25, 7, 18}};
+    }
+
+    @Test(expectedExceptions = AssertionError.class, dataProvider = "negativeWhileProvider")
+    public void negativeWhileTest(int a, int b) {
+        new Variant1().whileTask(a, b);
+    }
+
+    @DataProvider
+    public Object[][] negativeWhileProvider() {
+        return new Object[][]{{1, 2}, {10, 14}, {0,5}};
+    }
+
+
+    //////////////////////////////////////////
+
+
+    @Test(dataProvider = "arrayProvider")
+    public void testArrayTask(int[] A,  int[] B) {
+        assertEquals(new Variant1().arrayTask(A), B);
+    }
+
+
+    @DataProvider
+    public Object[][] arrayProvider() {
+        int[] inputArray = {1, 2, 3, 4, 5};
+        int[] outputArray = {1, 3, 5, 7, 9};
+        return new Object[][]{{inputArray, outputArray}};
+    }
+
+
+    ////////////////////////////////////////
+
+
+    @Test(dataProvider = "matrixProvider")
+    public void testMatrixTask(int[][] A, int[][] B){
+        assertEquals(new Variant1().matrixTask(A), B);
+    }
+    @DataProvider
+    public Object[][] matrixProvider() {
+        int[][] inputArray1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] outputArray1 = {{0, 0, 0}, {10, 10, 10}, {20,20,20}};
+        return new Object[][]{{inputArray1, outputArray1}};
+    }
+
+
+}
+
---
+++ Variant1.java
@@ -0,0 +1,159 @@
+package lab0;
+
+public class Variant1 {
+
+    public enum DAY_OF_WEEK{
+        MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+    }
+
+
+    /**
+     * Integer1.There is the distance L in centimeters.
+     *  Using the integer division operation is aimed at finding
+     *  the number of complete meters in it (1 m = 100 cm).
+     */
+
+    public  int integerTask(int L, int B) {
+        if (B == 0) {
+            throw new IllegalArgumentException("b shouldn't be equal 0");
+        }
+        return L / B;
+    }
+
+    /**
+     * Boolean1. There is an integer A.
+     * Check the truth: "The number A is positive."
+     */
+    public  boolean booleanTask(int A) {
+        return A > 0;
+
+    }
+
+    /**
+     * If1.There is an integer number. If it is positive, then add 1 to it
+     * otherwise don't change it. Print the received number.
+     */
+    public int ifTask(int A) {
+        if (A > 0) {
+            A++;
+        }
+        return A;
+    }
+
+
+    /**
+     * Case1.There is an integer number in the range 1-7.
+     * Output a line - the name of the day of the week
+     * corresponding to the corresponding number (1 - Monday, 2 - Tuesday, etc.).
+     */
+    public String caseTask(int n) {
+        assert n >= 1 && n <= 7 : "incorrect number of weekday";
+        switch (n) {
+            case 1:
+                return "Monday";
+            case 2:
+                return "Tuesday";
+            case 3:
+                return "Wednesday";
+
+            case 4:
+                return "Thursday";
+
+            case 5:
+                return "Friday";
+
+            case 6:
+                return "Saturday";
+
+            case 7:
+                return "Sunday";
+
+            default:
+                throw new IllegalArgumentException("there's no such day") ;
+        }
+    }
+
+//    public DAY_OF_WEEK caseTask(int number1){
+//        assert number1 >= 1 && number1 <= 7 : "incorrect number of weekday";
+//        return DAY_OF_WEEK.WEDNESDAY;
+//    }
+
+
+    /**
+     *
+     *For1. There are integer numbers K and N (N > 0).
+     * Output N times the number K.
+     */
+    public int forTask(int k, int n) {
+        assert n > 0: "n should be more than zero";
+
+        for (int i = 0; i < n; i++) {
+//            System.out.print(k);
+//            System.out.print(' ');
+        }
+//       System.out.println();
+        return n;
+    }
+
+
+    /**
+     * While1.There are positive numbers A and B (A > B).
+     * The maximum possible number of segments of length B (without overlays)
+     * is placed on a segment of length A. Without using the operation of
+     * multiplication and division find the length of the unoccupied part of segment A.
+     */
+    public  int whileTask(int A, int B) {
+
+        int temp = A;
+        if (temp < B) {
+            throw new AssertionError(" A should be more than B");
+        }
+        while (temp >= 0) {
+            temp -= B;
+        }
+        return A - B;
+    }
+
+    /**
+     * Array1.There is an integer number N (N > 0). Form and output an integer array of size N,
+     * containing N first additional odd numbers: 1, 3, 5, ... .
+     */
+
+    public int[] arrayTask(int[] array){
+        int n = array.length;
+        assert n > 0 : "n should be more than 0";
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = 1 + i * 2;
+        }
+//        for (int i = 0; i < n; ++i)
+//            System.out.println("a[" + i + "]=" + arr[i]);
+        return arr;
+    }
+
+
+
+    /**
+     *
+     * Matrix1. There are positive integer numbers M and N.
+     * Form an integer matrix of size M x N,
+     * in which all elements of the I-th row have the value 10*I (I = 1, …, M).
+     */
+    public int[][] matrixTask(int[][] array) {
+        int M = array.length, N = array[0].length;
+        assert M > 0 && N > 0 : "M and N should be more than 0";
+        int matrix[][] = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = 10 * i;
+            }
+        }
+        return matrix;
+    }
+
+
+    public static void main(String args[]) {
+    }
+
+
+}
